60988274f5b4464d37a15c3d6b3a17efb83c1463
Eric Bartusch <ericbartusch8@gmail.com>
2023-01-19 09:34:43 -0600

    TypeOverride for art cards

--- src/main/java/thePackmaster/cards/rippack/AbstractRippedArtCard.java
+++ src/main/java/thePackmaster/cards/rippack/AbstractRippedArtCard.java
@@ -7,9 +7,11 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thePackmaster.ThePackmaster;
-import thePackmaster.cards.AbstractPackmasterCard;
+import thePackmaster.patches.rippack.TypeOverridePatch;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,14 +20,21 @@ import static thePackmaster.SpireAnniversary5Mod.*;
 
 public abstract class AbstractRippedArtCard extends AbstractRipCard {
 
-    AbstractRippableCard sourceCard;
+    public AbstractRippableCard sourceCard;
     public static ShaderProgram shader = null;
     private static ArrayList<TooltipInfo> consumableTooltip;
 
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(makeID("Rip"));
+
     public AbstractRippedArtCard(String cardID, AbstractRippableCard sourceCard, CardColor color) {
-        super(cardID, sourceCard.cost, sourceCard.type, CardRarity.SPECIAL, CardTarget.NONE, color, getCardTextureString(sourceCard.cardID.replace(modID + ":", ""), sourceCard.type));
+        super(cardID, sourceCard.cost, CardType.STATUS, CardRarity.SPECIAL, CardTarget.NONE, color, getCardTextureString(sourceCard.cardID.replace(modID + ":", ""), sourceCard.type));
         this.sourceCard = sourceCard;
         setDisplayRarity(sourceCard.rarity);
+        if(sourceCard.type == CardType.ATTACK) {
+            TypeOverridePatch.setOverride(this, uiStrings.TEXT[2]);
+        } else if(sourceCard.type == CardType.SKILL) {
+            TypeOverridePatch.setOverride(this, uiStrings.TEXT[3]);
+        }
         exhaust = true;
     }
 
